844db5cb53ddbe89782c04e8a9dd20360902c921
thkummi <36160753+thkummi@users.noreply.github.com>
2021-08-16 14:32:48 +0200

    Update hb9cv-calc.c

--- hb9cv-calc.c
+++ hb9cv-calc.c
@@ -18,58 +18,86 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <stdio.h>
 
+void printunit (int unit_index){
+        switch(unit_index){
+        case 1: printf("mm\n");
+                break;
+        case 2: printf("inch\n");
+                break;
+        }
+
+}
+
+
 int main() {
-	
-	float f, lambda, l1, l2, l3, l4, l5, bdiam, raddiam;
 
-	printf("Please input an f in MHz value: e.G. 432.2 \n");
-    	scanf("%f", &f);
-    
-    	printf("Calculating for %f MHz\n", f);
-	
-	lambda = ( 30000000/f)/100;	
-	l1 = ( 0.92*lambda*5)/10;
-	l2 = ( lambda*5)/10;
-	l3 = ( lambda*1.25)/10;
-	l4 = ( lambda*1.25)/20;
-	l5 = ( lambda*1.35)/20;
-	bdiam = lambda*0.0072;
-	raddiam = lambda*0.0036;
+        float f, lambda, l1, l2, l3, l4, l5, bdiam, raddiam, unit_factor = 1;
+        int unit_index = 1;
+        printf("Please input an f in MHz value: e.G. 432.2 ");
+        scanf("%f", &f);
+        printf("Please choose (1)Metric or (2)Imperial units ");
+        scanf("%i", &unit_index);
 
-	printf(" Lambda is %f mm\n", lambda);
-	printf(" L1 Driven Element is %f mm\n", l1);
-	printf(" L2 Reflector is %f mm\n", l2);
-	printf(" L3 element spaceing is %f mm\n", l3);
-	printf(" L4 Matching on dr. element is %f mm\n", l4);
-	printf(" L5 Matching on reflector is %f mm\n", l5);
-	printf(" Boom Diam. is %f mm\n", bdiam);
-	printf(" Radiator Diam. is %f mm\n", raddiam);
-	
-	printf("\n");
-	printf("  connect feed on this side \n");
-	printf("  --------------------------L1 \n");
-	printf("          L4|--\\| \n");
-	printf("               || \n");	
-	printf("              L3| \n");
-	printf("                || \n");
-	printf("                | \\--|L5 \n");
-	printf(" ----------------------------L2 \n");
-	printf("\n");
-	printf("\n");
-	printf(" ===============================================\n");
-	printf("                        || \\     |\n");
-	printf("                        || C1 ___\\______\n");
-	printf("                        ||  \\––– /   ___|__\n");
-	printf("                        ||   |      /'  |  '\\ \n");
-	printf("                        ||---)------\\.__|__./\n");
-	printf("                        ||   |      |  50   |\n");
-	printf("                        ||   |      |  Ohm  |\n");
-	printf("\n");
-	printf(" C1 10m-Band 50pF 6m-Band 30pF 2m band 12pF \n");
-	printf("\n");
-	printf("\n");
-	
-	printf(" 73! \n");
-	
-	return 0;
+        switch(unit_index){
+        case 1: unit_factor = 1;
+                break;
+        case 2: unit_factor = 0.039; //for inch
+                break;
+        }
+
+        printf("Calculating for %f MHz\n", f);
+
+        lambda = ( 30000000/f)/100;
+        l1 = (( 0.92*lambda*5)/10)*unit_factor;
+        l2 = (( lambda*5)/10)*unit_factor;
+        l3 = (( lambda*1.25)/10)*unit_factor;
+        l4 = (( lambda*1.25)/20)*unit_factor;
+        l5 = (( lambda*1.35)/20)*unit_factor;
+        bdiam = (lambda*0.0072)*unit_factor;
+        raddiam = (lambda*0.0036)*unit_factor;
+
+        printf(" Lambda is %f mm\n", lambda);
+        printf(" L1 Driven Element is %f ", l1);
+        printunit(unit_index);
+        printf(" L2 Reflector is %f ", l2);
+        printunit(unit_index);
+        printf(" L3 element spaceing is %f ", l3);
+        printunit(unit_index);
+        printf(" L4 Matching on dr. element is %f ", l4);
+        printunit(unit_index);
+        printf(" L5 Matching on reflector is %f ", l5);
+        printunit(unit_index);
+        printf(" Boom Diam. is %f ", bdiam);
+        printunit(unit_index);
+        printf(" Radiator Diam. is %f ", raddiam);
+        printunit(unit_index);
+
+        printf("\n");
+        printf("  connect feed on this side \n");
+        printf("  --------------------------L1 \n");
+        printf("          L4|--\\| \n");
+        printf("               || \n");
+        printf("              L3| \n");
+        printf("                || \n");
+        printf("                | \\--|L5 \n");
+        printf(" ----------------------------L2 \n");
+        printf("\n");
+        printf("\n");
+        printf(" ===============================================\n");
+        printf("                        || \\     |\n");
+        printf("                        || C1 ___\\______\n");
+        printf("                        ||  \\____/   ___|__\n");
+        printf("                        ||   |      /'  |  '\\ \n");
+        printf("                        ||---)------\\.__|__./\n");
+        printf("                        ||   |      |  50   |\n");
+        printf("                        ||   |      |  Ohm  |\n");
+        printf("\n");
+        printf(" C1 10m-Band 50pF 6m-Band 30pF 2m band 12pF \n");
+        printf("\n");
+        printf("\n");
+        printf("Press Any Key to Continue/Quit \n");
+        _getch();
+        printf(" 73! \n");
+
+        return 0;
 }
