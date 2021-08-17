886a8d358503f237258e7264802a29fe60536628
thkummi <36160753+thkummi@users.noreply.github.com>
2021-08-17 16:54:36 +0200

    Update hb9cv-calc.c

--- hb9cv-calc.c
+++ hb9cv-calc.c
@@ -28,49 +28,58 @@ void printunit (int unit_index){
 
 }
 
+void set_freq(float *f){
+        float input;
+        printf("Please input an f in MHz value: e.G. 432.2 \n");
+        scanf("%f", &input);
+        *f = input;
+        printf("\n");
 
-int main() {
 
-        float f, lambda, l1, l2, l3, l4, l5, bdiam, raddiam, unit_factor = 1;
-        int unit_index = 1;
-        printf("Please input an f in MHz value: e.G. 432.2 ");
-        scanf("%f", &f);
-        printf("Please choose (1)Metric or (2)Imperial units ");
-        scanf("%i", &unit_index);
+}
 
-        switch(unit_index){
-        case 1: unit_factor = 1; break;
-        case 2: unit_factor = 0.039; break; //for inch
-        default: unit_factor = 1; unit_index = 1;  printf("Invalid input! Guessing! \n"); printf("\n"); break;
+void choose_units_system(float *unit_factor ,int *unit_index ){
+        int input;
+        printf("Please choose (1)Metric or (2)Imperial units \n");
+        scanf("%i", &input);
+        *unit_index = input;
+        printf("\n");
+        switch(*unit_index){
+        case 1: *unit_factor = 1; break;
+        case 2: *unit_factor = 0.039; break; //for inch
+        default: *unit_factor = 1; *unit_index = 1;  printf("Invalid input! Guessing! \n"); printf("\n"); break;
         }
-        
-        printf("Calculating for %f MHz\n", f);
+}
 
-        lambda = ( 30000000/f)/100;
-        l1 = (( 0.92*lambda*5)/10)*unit_factor;
-        l2 = (( lambda*5)/10)*unit_factor;
-        l3 = (( lambda*1.25)/10)*unit_factor;
-        l4 = (( lambda*1.25)/20)*unit_factor;
-        l5 = (( lambda*1.35)/20)*unit_factor;
-        bdiam = (lambda*0.0072)*unit_factor;
-        raddiam = (lambda*0.0036)*unit_factor;
+void hb9cv_calc (float *f, float *unit_factor, float *lambda, float *l1, float *l2, float *l3, float *l4, float *l5, float *bdiam, float *raddiam){
+        *lambda = ( 30000000 / *f) / 100;
+        *l1 = (( 0.92 * *lambda * 5) / 10) * *unit_factor;
+        *l2 = (( *lambda * 5) / 10) * *unit_factor;
+        *l3 = (( *lambda * 1.25) / 10) * *unit_factor;
+        *l4 = (( *lambda * 1.25) / 20) * *unit_factor;
+        *l5 = (( *lambda * 1.35) / 20) * *unit_factor;
+        *bdiam = ( *lambda * 0.0072) * *unit_factor;
+        *raddiam = ( *lambda * 0.0036) * *unit_factor;
+}
 
-        printf(" Lambda is %f mm\n", lambda);
-        printf(" L1 Driven Element is %f ", l1);
-        printunit(unit_index);
-        printf(" L2 Reflector is %f ", l2);
-        printunit(unit_index);
-        printf(" L3 element spaceing is %f ", l3);
-        printunit(unit_index);
-        printf(" L4 Matching on dr. element is %f ", l4);
-        printunit(unit_index);
-        printf(" L5 Matching on reflector is %f ", l5);
-        printunit(unit_index);
-        printf(" Boom Diam. is %f ", bdiam);
-        printunit(unit_index);
-        printf(" Radiator Diam. is %f ", raddiam);
-        printunit(unit_index);
-
+void hb9cv_print (float *f, float *unit_factor, float *lambda, float *l1, float *l2, float *l3, float *l4, float *l5, float *bdiam, float *raddiam, int *unit_index){
+        printf(" Calculated for %f mHz\n", *f);
+        printf("\n");
+        printf(" Lambda is %f mm\n", *lambda);
+        printf(" L1 Driven Element is %f ", *l1);
+        printunit(*unit_index);
+        printf(" L2 Reflector is %f ", *l2);
+        printunit(*unit_index);
+        printf(" L3 element spaceing is %f ", *l3);
+        printunit(*unit_index);
+        printf(" L4 Matching on dr. element is %f ", *l4);
+        printunit(*unit_index);
+        printf(" L5 Matching on reflector is %f ", *l5);
+        printunit(*unit_index);
+        printf(" Boom Diam. is %f ", *bdiam);
+        printunit(*unit_index);
+        printf(" Radiator Diam. is %f ", *raddiam);
+        printunit(*unit_index);
         printf("\n");
         printf("  connect feed on this side \n");
         printf("  --------------------------L1 \n");
@@ -94,8 +103,22 @@ int main() {
         printf(" C1 10m-Band 50pF 6m-Band 30pF 2m band 12pF \n");
         printf("\n");
         printf("\n");
-        /*printf("Press Any Key to Continue/Quit \n");
-        _getch();*/ //comment in on windows as requ
+}
+
+int main() {
+
+        float f, lambda, l1, l2, l3, l4, l5, bdiam, raddiam;
+        float unit_factor = 1;
+        int unit_index = 1;
+
+        set_freq(&f);
+
+        choose_units_system(&unit_factor, &unit_index);
+
+        hb9cv_calc(&f ,&unit_factor, &lambda, &l1, &l2, &l3, &l4, &l5, &bdiam, &raddiam);
+
+        hb9cv_print(&f ,&unit_factor, &lambda, &l1, &l2, &l3, &l4, &l5, &bdiam, &raddiam, &unit_index);
+
         printf(" 73! \n");
 
         return 0;
